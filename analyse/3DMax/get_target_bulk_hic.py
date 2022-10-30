5c7a7e2fdb3cf8b445d0f1cea22a25d7267a6ac9
liminghong.dev <lmh0066@outlook.com>
2022-10-30 14:54:29 +0800

    fig2&fig3

--- analyse/3DMax/get_target_bulk_hic.py
+++ analyse/3DMax/get_target_bulk_hic.py
@@ -11,8 +11,8 @@ train_set = np.load('/lmh_data/data/sclab/sclab/train_dataset.npy', allow_pickle
 eval_set = np.load('/lmh_data/data/sclab/sclab/eval_dataset.npy', allow_pickle=True)
 data_set = np.append(train_set, eval_set)
 
-gene_name, cell_type = 'SLC1A2', 'Astro'
-# gene_name, cell_type = 'PDGFRA', 'OPC'
+# gene_name, cell_type = 'SLC1A2', 'Astro'
+gene_name, cell_type = 'PDGFRA', 'Astro'
 # gene_name, cell_type = 'MBP', 'ODC'
 
 
@@ -21,12 +21,18 @@ def show_by_cell_type(cell_type):
     for _data in data_set:
         if _data['cell_type'] != cell_type:
             continue
+        if _data['identity'] != 'truth':
+            continue
         num += 1
         _hic = _data['scHiC'][gene_name].astype(float)
         if _hics is None:
             _hics = _hic.copy()
         else:
             _hics += _hic
+        if num >= 250:
+            break
+    if num < 250:
+        raise Exception()
     return _hics/num
 mat = array2mat(show_by_cell_type(cell_type))
 mat = mat / mat.max() * 1000
@@ -39,11 +45,11 @@ with open(ftmp.name, 'w') as f:
         _strs.append(','.join([str(j) for j in mat[i]])+'\n')
     f.writelines(_strs)
 
-out_dir = gene_name
+out_dir = gene_name + '_' + cell_type
 os.makedirs(out_dir, exist_ok=True)
 with open('parameters.txt', 'w') as f:
     f.writelines([
-        'NUM = 1\n', 'OUTPUT_FOLDER = {}\n'.format(gene_name), 'INPUT_FILE = {}\n'.format(ftmp.name),
+        'NUM = 1\n', 'OUTPUT_FOLDER = {}\n'.format(out_dir), 'INPUT_FILE = {}\n'.format(ftmp.name),
         'VERBOSE = true\n', 'LEARNING_RATE = 1\n', 'MAX_ITERATION = 10000\n'
     ])
 
